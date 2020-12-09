126afdaffdb230c2aa495144941adaaf590bd056
Andrea Leo <andrealeo83@users.noreply.github.com>
2020-12-09 14:44:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ const url = require('url');
 
  
 http.createServer(function (req, res) {
+ 
+ //cors start
+ const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+    'Access-Control-Max-Age': 2592000, // 30 days
+    /** add other headers as per requirement */
+  };
+
+  if (req.method === 'OPTIONS') {
+    res.writeHead(204, headers);
+    res.end();
+    return;
+  }
+  //cors end
+ 
   const queryObject = url.parse(req.url,true).query;
 
   var userId = queryObject.id;
